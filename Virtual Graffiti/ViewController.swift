8b76d1e11775a47cc13c6804fe9e515520d02097
Hunter Salada <hsalada@Hunters-MacBook-Pro.local>
2020-03-06 20:41:04 -0500

    added plane detection and the ability to place a 2D image on the detected plane as per commented tutorial

--- Virtual Graffiti/ViewController.swift
+++ Virtual Graffiti/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var stickersButton: UIButton!
     @IBOutlet weak var placeButton: UIButton!
     
+    var grids = [Grid]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,19 +29,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = false
         
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
         
+        // allowing a tap to place things
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        sceneView.addGestureRecognizer(gestureRecognizer)
+        
     }
+        
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .vertical
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -97,5 +107,60 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     // check before segue for passing variables to new views, checking for existing classes, etc.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    
+    
+    // THE FOLLOWING FUNCTIONS ARE FOR PLANE DETECTION AND PLACING 2D IMAGES
+    // from the tutorial at https://mobile-ar.reality.news/how-to/arkit-101-place-2d-images-like-painting-photo-wall-augmented-reality-0187598/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        let grid = Grid(anchor: planeAnchor)
+        self.grids.append(grid)
+        node.addChildNode(grid)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        let grid = self.grids.filter { grid in
+            return grid.anchor.identifier == planeAnchor.identifier
+            }.first
+
+        guard let foundGrid = grid else {
+            return
+        }
+
+        foundGrid.update(anchor: planeAnchor)
+    }
+    
+    @objc func tapped(gesture: UITapGestureRecognizer) {
+        // Get 2D position of touch event on screen
+        let touchPosition = gesture.location(in: sceneView)
+
+        // Translate those 2D points to 3D points using hitTest (existing plane)
+        let hitTestResults = sceneView.hitTest(touchPosition, types: .existingPlaneUsingExtent)
+
+        // Get hitTest results and ensure that the hitTest corresponds to a grid that has been placed on a wall
+        guard let hitTest = hitTestResults.first, let anchor = hitTest.anchor as? ARPlaneAnchor, let gridIndex = grids.firstIndex(where: { $0.anchor == anchor }) else {
+            return
+        }
+        addSticker(hitTest, grids[gridIndex])
+    }
+    
+    func addSticker(_ hitResult: ARHitTestResult, _ grid: Grid) {
+        // 1.
+        let planeGeometry = SCNPlane(width: 0.2, height: 0.35)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "mona-lisa")
+        planeGeometry.materials = [material]
+
+        // 2.
+        let stickerNode = SCNNode(geometry: planeGeometry)
+        stickerNode.transform = SCNMatrix4(hitResult.anchor!.transform)
+        stickerNode.eulerAngles = SCNVector3(stickerNode.eulerAngles.x + (-Float.pi / 2), stickerNode.eulerAngles.y, stickerNode.eulerAngles.z)
+        stickerNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+
+        sceneView.scene.rootNode.addChildNode(stickerNode)
+        grid.removeFromParentNode()
     }
 }
