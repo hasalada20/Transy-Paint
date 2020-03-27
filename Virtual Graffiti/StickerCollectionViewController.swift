df735eb0efca124a5216bae16b7c1cf92cc11721
Hunter Salada <hsalada@Hunters-MacBook-Pro.local>
2020-03-27 18:56:20 -0400

    uploading what I've done so far(not much but I need to transfer

--- Virtual Graffiti/StickerCollectionViewController.swift
+++ Virtual Graffiti/StickerCollectionViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 private let reuseIdentifier = "Cell"
+private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
 
 class StickerCollectionViewController: UICollectionViewController {
 
