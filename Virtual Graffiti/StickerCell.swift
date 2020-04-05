0e7aa8e41a108c59d2ec167dcb73dc852438764f
Hunter Salada <huntersalada@Hunters-16-Inches.local>
2020-04-05 18:02:02 -0400

    added second sticker and selected it instead of mona lisa

--- Virtual Graffiti/StickerCell.swift
+++ Virtual Graffiti/StickerCell.swift
@@ -4,10 +4,14 @@
 //
 //  Created by Hunter Salada on 3/27/20.
 //  Copyright © 2020 hunter. All rights reserved.
-//
+//  Created by following the tutorial here: https://www.hackingwithswift.com/read/10/2/designing-uicollectionview-cells
 
 import UIKit
 
+
+
 class StickerCell: UICollectionViewCell {
+   
+    @IBOutlet var imageView: UIImageView!
     
 }
