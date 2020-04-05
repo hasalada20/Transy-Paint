b6fb8581f899fa0dc8fd676f6d094aa9e7c7ea85
Hunter Salada <huntersalada@Hunters-16-Inches.local>
2020-04-05 18:01:31 -0400

    added a second sticker and selected it instead of mona lisa

---
+++ Virtual Graffiti/StickerCell.swift
@@ -0,0 +1,13 @@
+//
+//  StickerCell.swift
+//  Virtual Graffiti
+//
+//  Created by Hunter Salada on 3/27/20.
+//  Copyright © 2020 hunter. All rights reserved.
+//
+
+import UIKit
+
+class StickerCell: UICollectionViewCell {
+    
+}
