0ac4e0ca4fc3b01d717eec39dcf121b6a36d07f3
amlamb20 <amlamb20@transy.edu>
2020-04-18 10:44:48 -0400

    navbar disappears on cam view

--- Virtual Graffiti/ViewController.swift
+++ Virtual Graffiti/ViewController.swift
@@ -53,6 +53,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        // Hide navigation controller
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -60,6 +63,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+        
+        // Stop hiding navigation controller
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
     // MARK: - ARSCNViewDelegate
